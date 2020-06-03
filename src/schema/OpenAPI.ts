61164d41a4cd03c768196457ec802c977a82f319
Petr Flaks <petr@flaks.dev>
2020-06-03 13:36:49 +0300

    Add missing fields to OpenAPI type

--- src/schema/OpenAPI.ts
+++ src/schema/OpenAPI.ts
@@ -2,13 +2,19 @@ import { Info } from './Info';
 import { Server } from './Server';
 import { Paths } from './Paths';
 import { Components } from './Components';
+import { ExternalDocumentation } from './ExternalDocumentation';
+import { SecurityRequirement } from './SecurityRequirement';
+import { Tag } from './Tag';
 
 export const openapi = '3.0.3' as const;
 
 export interface OpenAPI {
   components?: Components;
+  externalDocs?: ExternalDocumentation;
   info: Info;
   openapi: typeof openapi;
   paths: Paths;
+  security?: SecurityRequirement[];
   servers?: Server[];
+  tags?: Tag[];
 }
